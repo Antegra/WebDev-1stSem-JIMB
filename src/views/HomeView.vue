8d53fbef8f01406e1b1daa1aa5b1aa25415dd2c2
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2022-10-30 14:48:50 +0100

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,20 +6,9 @@
   </div>
 </template>
 
-<<<<<<< Updated upstream
-<script>
-export default {
-  setup () {
-    
-
-    return {}
-  }
-}
-=======
 <script setup>
-import Multi_step_form from '../components/multi_step_form.vue';
 
->>>>>>> Stashed changes
+
 </script>
 
 <style lang="scss" scoped>
