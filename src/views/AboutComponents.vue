0d3d5846c243be6eb28732a959d47b706a056cb0
Mads W <madswandersen@hotmail.com>
2022-10-31 11:03:39 +0100

    Button compents PREV|NEXT

---
+++ src/views/AboutComponents.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="buttons">
+        <MyButton @click="handleBack" :arrowFront="false" buttonText="Tilbage" />
+        <MyButton @click="handleNext" :arrowFront="true" buttonText="Næste" />
+        </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import MyButton from "../components/buttons.vue"
+  
+  
+  
+  </script>
+
+
+
+<script>
+export default {
+  components: {
+    MyButton
+  },
+  methods: {
+        handleNext() {
+            console.log("Next");
+        },
+        handleBack() {
+            console.log("Back");
+        },
+    }
+}
+</script>
+  
+  
+  <style lang="scss" scoped>
+    .buttons {
+        display:flex;
+    }
+
+
+  </style>
+  
