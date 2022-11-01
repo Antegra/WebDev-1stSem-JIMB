362de22a1b7b85ed7f72b1cef0932659fcee28d7
idax6797 <64838676+idax6797@users.noreply.github.com>
2022-11-01 13:41:43 +0100

    Changed the style a bit

--- src/components/header.vue
+++ src/components/header.vue
@@ -29,29 +29,40 @@ import { RouterLink, RouterView } from "vue-router";
 <style lang="scss" scoped>
 @import "../assets/colors.scss";
 
-.wrapper {
-  nav {
-    display: flex;
-    align-items: baseline;
-    justify-content: space-between;
-
-    .logo {
-      max-width: 143px;
-    }
-    .main-navigation {
-      a {
-        color: $Midnight-Green;
-        margin: 0 20px;
-        text-decoration: none;
-
-        .material-symbols-outlined {
-          position: relative;
-          top: 5px;
-          font-variation-settings:
-          'FILL' 1,
-          'wght' 400,
-          'GRAD' 0,
-          'opsz' 100
+header {
+  display: flex;
+  flex: row;
+  justify-content: center;
+  .wrapper {
+    nav {
+      display: flex;
+      align-items: baseline;
+      justify-content: space-between;
+      padding-top: 30px;
+  
+      .logo {
+        max-width: 143px;
+      }
+      .main-navigation {
+        padding-left: 5rem;
+        a {
+          color: $Midnight-Green;
+          margin: 0 20px;
+          text-decoration: none;
+  
+          &:hover, &:active {
+            text-decoration: underline;
+          }
+  
+          .material-symbols-outlined {
+            position: relative;
+            top: 7px;
+            font-variation-settings:
+            'FILL' 1,
+            'wght' 400,
+            'GRAD' 0,
+            'opsz' 100
+          }
         }
       }
     }
