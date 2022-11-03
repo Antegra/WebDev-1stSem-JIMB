e8ab51fd5a441f902d88411e71e45e0f7f3e2486
idax6797 <64838676+idax6797@users.noreply.github.com>
2022-11-03 11:21:00 +0100

    Update header

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,20 +1,38 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
+
+function toggleNav() {
+  var x = document.getElementById("main-navigation")
+  var y = document.getElementById("toggleicon");
+  if (x.className === "main-navigation" || y.className === "toggleicon") {
+    x.className += "-responsive ";
+    y.className += "-active ";
+  } else {
+    x.className = "main-navigation";
+    y.className += "toggleicon";
+  }
+  console.log('menuen virker')
+}
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <nav>
+      <nav class="nav" id="nav">
         <RouterLink to="/">
           <img class="logo" src="../assets/img/ucl-logo.png" alt="UCLs logo" />
         </RouterLink>
-        <div class="main-navigation">
-          <RouterLink to="/">Hjem</RouterLink>
-          <RouterLink to="/newcase">Ny sag</RouterLink>
-          <RouterLink to="/oldcases">Gamle sager</RouterLink>
-          <RouterLink to="/data">Data</RouterLink>
-          <RouterLink to="/settings">
+        <div class="main-navigation" id="main-navigation">
+          <a href="javascript:void(0);" @click="toggleNav" >
+            <span class="material-symbols-outlined toggleicon" id="toggleicon">
+              menu
+            </span>
+          </a>
+          <RouterLink class="navigation-link" to="/">Hjem</RouterLink>
+          <RouterLink class="navigation-link" to="/newcase">Ny sag</RouterLink>
+          <RouterLink class="navigation-link" to="/oldcases">Gamle sager</RouterLink>
+          <RouterLink class="navigation-link" to="/data">Data</RouterLink>
+          <RouterLink class="navigation-link" to="/settings">
             <span class="material-symbols-outlined">
               settings
             </span>
@@ -28,44 +46,84 @@ import { RouterLink, RouterView } from "vue-router";
 
 <style lang="scss" scoped>
 @import "../assets/colors.scss";
-
 header {
   display: flex;
   flex: row;
   justify-content: center;
   .wrapper {
+    width: 100%;
     nav {
       display: flex;
       align-items: baseline;
-      justify-content: space-between;
-      padding-top: 30px;
+      justify-content: space-evenly;
+      padding-top: 2rem;
   
       .logo {
         max-width: 143px;
       }
       .main-navigation {
-        padding-left: 5rem;
+        overflow: hidden;
         a {
           color: $Midnight-Green;
           margin: 0 20px;
           text-decoration: none;
   
-          &:hover, &:active {
-            text-decoration: underline;
-          }
-  
           .material-symbols-outlined {
             position: relative;
-            top: 7px;
+            top: 5px;
             font-variation-settings:
-            'FILL' 1,
+            'FILL' 0,
             'wght' 400,
             'GRAD' 0,
             'opsz' 100
           }
         }
+        .toggleicon {
+            display: none;
+          }
       }
+    }
+    
+    //Mobileview
+    @media screen and (max-width: 620px) {
+
+      nav {
+        padding: 2rem;
+        justify-content: space-between;
+        
+        .main-navigation {
+          .navigation-link {
+            display: none;
+          }
+
+          .toggleicon {
+            display: inline;
+        }
+      }
+      .main-navigation-responsive {
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 100vh;
+        width: 50vw;
+        background-color: $Columbia-blue;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+
+        .navigation-link {
+          padding: 1rem 0;
+          color: $Midnight-Green;
+          text-decoration: none;
+        }
+      }
+    }
     }
   }
 }
+
+
+  
+
 </style>
