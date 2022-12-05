25f81956fc23b41664d01d497f40c637bb7bdad6
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2022-12-05 22:17:37 +0100

    Update OldcasesView.vue

--- src/views/OldcasesView.vue
+++ src/views/OldcasesView.vue
@@ -5,7 +5,7 @@ let cases = ref([]);
 
 onBeforeMount(async () => {
 
-  const fetchedCases = await fetch('https://localhost:7196/api/Case/1')
+  const fetchedCases = await fetch('https://uclssapitest.azurewebsites.net/api/Case')
     .then((fetchedCases) => fetchedCases.json())
   for (let i = 0; i < fetchedCases.length; i++) {
     cases.value.push(fetchedCases[i])
