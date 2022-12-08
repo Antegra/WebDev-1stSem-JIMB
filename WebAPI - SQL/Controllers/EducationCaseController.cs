ac78f64d3cd65c32672706125c6a9dce89379266
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2022-12-08 13:58:29 +0100

    its back

---
+++ WebAPI - SQL/Controllers/EducationCaseController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using WebAPI___SQL.Models;
+
+namespace WebAPI___SQL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EducationCaseController : ControllerBase
+    {
+        string constr = "Server=tcp:uclssdemo.database.windows.net,1433;Initial Catalog=studieservice;Persist Security Info=False;User ID=awesomegroupx;Password=t4prSlX1JCZ0Ujv;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        // GET: api/EducationCase
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Education_case>>> GetAllEducationCases()
+        {
+            List<Education_case> education_cases = new List<Education_case>();
+            string query = "SELECT * FROM edu_case";
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            education_cases.Add(new Education_case
+                            {
+                                edu_id = Convert.ToInt32(sdr["edu_id"]),
+                                case_id = Convert.ToInt32(sdr["case_id"]),
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+
+            return education_cases;
+        }
+
+        // POST: api/EducationCase
+        [HttpPost]
+        public async Task<ActionResult<Education_case>> PostEducationCases(Education_case Education_case)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                //inserting Patient data into database
+                string query = "INSERT INTO edu_case (edu_id, case_id) VALUES (@edu_id, @case_id)";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Connection = con;
+                    cmd.Parameters.AddWithValue("@edu_id", Education_case.edu_id);
+                    cmd.Parameters.AddWithValue("@case_id", Education_case.case_id);
+                    con.Open();
+                    int i = cmd.ExecuteNonQuery();
+                    if (i > 0)
+                    {
+                        return Ok();
+                    }
+                    con.Close();
+                }
+            }
+            return BadRequest();
+
+        }
+
+        // DELETE: api/EducationCase/5
+        [HttpDelete("{edu_id}, {case_id}")]
+        public async Task<IActionResult> DeleteEducationCases(int edu_id, int case_id)
+        {
+
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string query = "DELETE FROM edu_case WHERE edu_id ='" + edu_id + "' AND case_id ='" + case_id + "'";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    con.Open();
+                    int i = cmd.ExecuteNonQuery();
+                    if (i > 0)
+                    {
+                        return NoContent();
+                    }
+                    con.Close();
+                }
+            }
+            return BadRequest();
+        }
+
+    }
+}
---
+++ WebAPI - SQL/Models/Case.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using WebAPI___SQL.Models;
+
+namespace WebAPI___SQL.Models
+{
+	public class Case
+	{
+        public int case_id { get; set; }
+        public DateTime month { get; set; }
+        public string name { get; set; }
+        public string supervisor { get; set; }
+        public string sex { get; set; }
+        public string length { get; set; }
+        public string education { get; set; }
+        public string location { get; set; }
+        public string subject { get; set; }
+        public int user_id { get; set; }
+        public int sex_id { get; set; }
+        public int duration_id { get; set; }
+        public int type_id { get; set; }
+        public int edu_id { get; set; }
+        public int location_id { get; set; }
+        public int subject_id { get; set; }
+        public int primeEdu { get; set; }
+        public bool niveau { get; set; }
+        public bool nationality { get; set; }
+    }
+}
+
